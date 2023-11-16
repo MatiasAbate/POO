eeddee67b8397561062e4e8161e6901fa2f6611e
MatiasAbate <143027873+MatiasAbate@users.noreply.github.com>
2023-11-16 16:02:09 -0300

    Update Sede.java

--- src/TPO/Sede.java
+++ src/TPO/Sede.java
@@ -13,7 +13,41 @@ public class Sede {
         this.provincia = provincia;
     }
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	public List<Laboratorio> getLaboratorios() {
+		return laboratorios;
+	}
+
+	public void setLaboratorios(List<Laboratorio> laboratorios) {
+		this.laboratorios = laboratorios;
+	}
+
+	public Gerente getGerente() {
+		return gerente;
+	}
+
+	public void setGerente(Gerente gerente) {
+		this.gerente = gerente;
+	}
+
     // Getters y setters
+    
+    
 
     // Otros métodos si son necesarios
 
